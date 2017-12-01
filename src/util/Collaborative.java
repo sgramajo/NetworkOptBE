22a15356288452f1f15fcb9987b18f03f1730bc0
Rebeca Amaya <rebeca_amaya@bellsouth.net>
2017-12-01 08:42:55 -0500

    initial implementation of recommender system

--- src/util/Collaborative.java
+++ src/util/Collaborative.java
@@ -110,13 +110,10 @@ public class Collaborative {
 	}
 
 	// Class to implement collaborative filter
-	public static void calculateRatings(){
-
-		//printMatrix(reviewMap);
+	public static HashMap<String, HashMap<String, Double>> calculateRatings(){
 
 		// Row and column variables
 		int itemCount = reviewMap.size();
-		int userCount;
 
 		// Create arraylist matrix of items and their reviews by users
 		HashMap<String, HashMap<String, Double>> ratings = new HashMap<>(itemCount);
@@ -130,7 +127,6 @@ public class Collaborative {
 				}
 			});
 		});
-		userCount = userList.size();
 
 		// Initialilze ratings matrix, aka set all reviews to 0.0
 		// Then retrive ratigs for all reviews for each item
@@ -143,29 +139,28 @@ public class Collaborative {
 			});
 		});
 
-		// Debugger print
-//		ratings.forEach((itemID, reviewHash) -> {
-//			reviewHash.forEach((userID, rating) -> {
-//				if(rating > 0.0)
-//					System.out.print(rating + " ");
-//			});
-//			System.out.println();
-//		});
-
 		// Begin calculations to estimate missing ratings
 
 		// Step 1: normalize ratings
-		HashMap<String, HashMap<String, Double>> normalizedRatings = normalizeRatings(ratings, itemCount, userCount, userList);
+		HashMap<String, HashMap<String, Double>> normalizedRatings = normalizeRatings(ratings, itemCount, userList);
+
+		// Step 2: calculate centered cosine similarity
+		HashMap<String, HashMap<String, Double>> similarityMatrix = calculateSimilarity(normalizedRatings, itemCount, userList);
+
+		// Step 3: estimate missing ratings
+		item_itemCollaborativeFilter(ratings, similarityMatrix);
+
+		return ratings;
 
 	}
 
 	// Normalize ratings by subtracting average item rating for each item (row mean)
 	// Makes 0 the average rating for an item
-	private static HashMap<String, HashMap<String, Double>> normalizeRatings(HashMap<String, HashMap<String, Double>> ratings, int itemCount, int userCount, ArrayList<String> userList){
+	private static HashMap<String, HashMap<String, Double>> normalizeRatings(HashMap<String, HashMap<String, Double>> ratings, int itemCount, ArrayList<String> userList){
 
 		HashMap<String, HashMap<String, Double>> normalizedRatings = new HashMap<>(itemCount);
-		normalizedRatings.forEach((itemID, reviewLst) ->{
-			ratings.put(itemID, new HashMap<String, Double>(){{
+		ratings.forEach((itemID, reviewLst) ->{
+			normalizedRatings.put(itemID, new HashMap<String, Double>(){{
 				userList.forEach(user -> put(user, 0.0));
 			}});
 		});
@@ -196,4 +191,108 @@ public class Collaborative {
 		return normalizedRatings;
 	}
 
+	// Calculate the centered cosine similarity for all items
+	private static HashMap<String, HashMap<String, Double>> calculateSimilarity(HashMap<String, HashMap<String, Double>> normalizedRatings, int itemCount, ArrayList<String> userLst){
+
+		HashMap<String, HashMap<String, Double>> similarityMatrix = new HashMap<>(itemCount);
+
+		// Initialize all similarities to 0.0
+		normalizedRatings.forEach((itemID, reviewLst) ->{
+			similarityMatrix.put(itemID, new HashMap<String, Double>(){{
+				normalizedRatings.forEach((item, list) -> put(item, 0.0));
+			}});
+		});
+
+		for(Map.Entry<String, HashMap<String, Double>> item1RatingEntry : normalizedRatings.entrySet()){
+			for(Map.Entry<String, HashMap<String, Double>> item2RatingEntry : normalizedRatings.entrySet()){
+
+				double similarity, dotProduct, item1Magnitude, item2Magnitude;
+				dotProduct = item1Magnitude = item2Magnitude = 0.0;
+
+				for(String user : userLst){
+					// dot product is almost always 0 ...
+					dotProduct += normalizedRatings.get(item1RatingEntry.getKey()).get(user) * normalizedRatings.get(item2RatingEntry.getKey()).get(user);
+					item1Magnitude += normalizedRatings.get(item1RatingEntry.getKey()).get(user) * normalizedRatings.get(item1RatingEntry.getKey()).get(user);
+					item2Magnitude += normalizedRatings.get(item2RatingEntry.getKey()).get(user) * normalizedRatings.get(item2RatingEntry.getKey()).get(user);
+				}
+
+				double denominator = Math.sqrt(item1Magnitude) * Math.sqrt(item2Magnitude);
+				similarity = (denominator != 0 ? dotProduct / denominator : 0.0);
+				similarityMatrix.get(item1RatingEntry.getKey()).put(item2RatingEntry.getKey(), similarity);
+
+			}
+		}
+
+		return similarityMatrix;
+	}
+
+	// Estimate the missing ratings using an item-item collaborative filter
+	private static void item_itemCollaborativeFilter(HashMap<String, HashMap<String, Double>> ratings, HashMap<String, HashMap<String, Double>> similarityMatrix){
+
+		// Get item neighborhood for all items in descending order based on similarity
+		HashMap<String, ArrayList<Item>> itemNeighborhood = new HashMap<>();
+		for(Map.Entry<String, HashMap<String, Double>> itemSimilarityEntry : similarityMatrix.entrySet()){
+
+			ArrayList<Item> neighborhood = new ArrayList<>();
+
+			for(Map.Entry<String, Double> item2SimilarityEntry : itemSimilarityEntry.getValue().entrySet()){
+				if(itemSimilarityEntry.getKey() != item2SimilarityEntry.getKey()){ // dont add item to its own neighborhood
+					neighborhood.add(new Item(item2SimilarityEntry.getKey(), item2SimilarityEntry.getValue()));
+				}
+			}
+
+			// Sort the item neighborhood
+			Collections.sort(neighborhood, new CustomComparator());
+			Collections.reverse(neighborhood);
+			itemNeighborhood.put(itemSimilarityEntry.getKey(), neighborhood);
+		}
+
+		// summations of similarity
+		double predictedRating, similarityByRating, similarity;
+
+		// Traverse ratings matric and estimate missing ratings
+		for(Map.Entry<String, HashMap<String, Double>> itemRatingEntry : ratings.entrySet()){
+			for(Map.Entry<String, Double> userRatingEntry : itemRatingEntry.getValue().entrySet()){
+
+				// If we have a missing rating
+				if(userRatingEntry.getValue() == 0.0){
+
+					// reset summations of similarity
+					similarityByRating = similarity = 0.0;
+
+					// Pick the top N most similar items to the current item that have been rated by the current user
+					int n = 0;
+					for(Item neighbor : itemNeighborhood.get(itemRatingEntry.getKey())){
+						if(ratings.get(itemRatingEntry.getKey()).get(userRatingEntry.getKey()) != 0.0){
+							similarityByRating += neighbor.similarity * ratings.get(itemRatingEntry.getKey()).get(userRatingEntry.getKey());
+							similarity += neighbor.similarity;
+							n++;
+						}
+						if(n == N) break;
+					}
+
+					predictedRating = (similarity != 0 ? similarityByRating / similarity : 0.0);
+					ratings.get(itemRatingEntry.getKey()).put(userRatingEntry.getKey(), (predictedRating < 1.0 ? 1.0 : (predictedRating > 5.0 ? 5.0 : predictedRating)));
+				}
+			}
+		}
+	}
+}
+
+class Item{
+	Double similarity;
+	String item;
+
+	public Item(String item, double similarity){
+		this.item = item;
+		this.similarity = similarity;
+	}
+}
+
+class CustomComparator implements Comparator<Item>{
+
+	@Override
+	public int compare(Item o1, Item o2) {
+		return o1.similarity.compareTo(o2.similarity);
+	}
 }
